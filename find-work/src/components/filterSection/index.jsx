1963f8766df3d055ad78ed9309ec0eb17f54427d
mayurk224 <mayurkamble0250@gmail.com>
2026-07-01 09:30:26 +0530

    refactor: improve filter section and fix header
    Extract job type and salary range options into reusable arrays, then replace hardcoded form inputs in FilterSection with mapped JSX to reduce repetition and improve maintainability. Fix header component to use React-compliant className instead of plain class attributes, and add the associated CSS styles for the header.

--- find-work/src/components/filterSection/index.jsx
+++ find-work/src/components/filterSection/index.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 import './index.css'
 
 const FilterSection = () => {
+  const jobTypes = [
+    { value: "Full Time", label: "Full Time" },
+    { value: "Part Time", label: "Part Time" },
+    { value: "Internship", label: "Internship" },
+    { value: "Freelance", label: "Freelance" }
+  ];
+  const salaryRanges = [
+    { value: "", label: "All", defaultChecked: true },
+    { value: "10-lpa-and-above", label: "10 LPA and above" },
+    { value: "20-lpa-and-above", label: "20 LPA and above" },
+    { value: "30-lpa-and-above", label: "30 LPA and above" },
+    { value: "40-lpa-and-above", label: "40 LPA and above" }
+  ]
   return (
     <div className="filter-section">
       <h5 className="filter-title">Filter</h5>
@@ -10,24 +23,18 @@ const FilterSection = () => {
         <div className="filter-item">
           <div className="filter-label">Job Type</div>
           <div className="filter-select">
-            <div className="checkbox-group">
-              <label className="checkbox-option">
-                <input type="checkbox" name="job-type" value="Full Time" />
-                <span>Full Time</span>
-              </label>
-              <label className="checkbox-option">
-                <input type="checkbox" name="job-type" value="Part Time" />
-                <span>Part Time</span>
-              </label>
-              <label className="checkbox-option">
-                <input type="checkbox" name="job-type" value="Internship" />
-                <span>Internship</span>
-              </label>
-              <label className="checkbox-option">
-                <input type="checkbox" name="job-type" value="Freelance" />
-                <span>Freelance</span>
-              </label>
-            </div>
+            {(() => {
+              return (
+                <div className="checkbox-group">
+                  {jobTypes.map((type) => (
+                    <label key={type.value} className="checkbox-option">
+                      <input type="checkbox" name="job-type" value={type.value} />
+                      <span>{type.label}</span>
+                    </label>
+                  ))}
+                </div>
+              );
+            })()}
           </div>
         </div>
       </div>
@@ -36,26 +43,17 @@ const FilterSection = () => {
         <div className="filter-label">Salary Range</div>
         <div className="filter-select">
           <div className="radio-group">
-            <label className="radio-option">
-              <input type="radio" name="salary-range" value="" defaultChecked />
-              <span>All</span>
-            </label>
-            <label className="radio-option">
-              <input type="radio" name="salary-range" value="10-lpa-and-above" />
-              <span>10 LPA and above</span>
-            </label>
-            <label className="radio-option">
-              <input type="radio" name="salary-range" value="20-lpa-and-above" />
-              <span>20 LPA and above</span>
-            </label>
-            <label className="radio-option">
-              <input type="radio" name="salary-range" value="30-lpa-and-above" />
-              <span>30 LPA and above</span>
-            </label>
-            <label className="radio-option">
-              <input type="radio" name="salary-range" value="40-lpa-and-above" />
-              <span>40 LPA and above</span>
-            </label>
+            {salaryRanges.map((range) => (
+              <label key={range.value || "all"} className="radio-option">
+                <input
+                  type="radio"
+                  name="salary-range"
+                  value={range.value}
+                  defaultChecked={range.defaultChecked || false}
+                />
+                <span>{range.label}</span>
+              </label>
+            ))}
           </div>
         </div>
       </div>
--- find-work/src/components/header/index.jsx
+++ find-work/src/components/header/index.jsx
@@ -16,12 +16,12 @@ const Header = () => {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
                             <NavLink to="/" className="nav-link" aria-current="page">Home</NavLink>
                         </li>
-                        <li class="nav-item">
+                        <li className="nav-item">
                             <NavLink to="/jobs" className="nav-link">Jobs</NavLink>
                         </li>
 
